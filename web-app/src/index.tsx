f8a9c9f713df1192b4637b744c6f92a5e9e965fe
MATTOP\Matthew <m.durkin92@gmail.com>
2021-10-26 21:39:59 +0100

    Add MoralisProvider

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider
+      appId={process.env.REACT_APP_MORALIS_APPLICATION_ID || ""}
+      serverUrl={process.env.REACT_APP_MORALIS_SERVER_URL || ""}
+    >
+      <App />
+    </MoralisProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
